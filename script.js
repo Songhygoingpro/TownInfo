3fdbde7eb34e3a7e197987e799ada30b0a59e1e4
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-05-27 15:52:50 +0700

    'first

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+  document.querySelectorAll('[data-aos="reveal-right"]').forEach(function (element) {
+    element.addEventListener('transitionend', function () {
+      if (element.classList.contains('aos-animate')) {
+        setTimeout(function () {
+          element.classList.add('hold');
+          element.nextElementSibling.style.visibility = 'visible';
+        }, 0); 
+      }
+    });
+  });
+});
+
+  let lastScrollTop = 0;
+  const header = document.querySelector('.header');
+
+  window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+
+      header.style.top = '-75px'; 
+    } else {
+
+      header.style.top = '0';
+    }
+
+    lastScrollTop = scrollTop;
+  });
